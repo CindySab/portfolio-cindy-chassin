425be628595349c376d85f85bdea05661ce3cdd9
CindySab <149073779+CindySab@users.noreply.github.com>
2024-06-12 18:01:57 +0200

    icon skills page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
             <nav>
                 <CustomLink href="/" title="Home" className='mr-4'/>
                 <CustomLink href="/about" title="About" className='mx-4'/>
-                <CustomLink href="/projets" title="Projets" className='mx-4'/>
+                <CustomLink href="/projects" title="Projects" className='mx-4'/>
                 <CustomLink href="/skills" title="Skills" className='ml-4'/>
             </nav>
             <nav className='flex items-center justify-center flex-wrap'>
